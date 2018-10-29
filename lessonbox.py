4d86fa6996a9e542b95853fbaa9d96f79b5b163a
bsantero <brian.santero@gmail.com>
2018-10-29 15:09:30 -0400

    clean up script

--- lessonbox.py
+++ lessonbox.py
@@ -1,12 +1,7 @@
 import sys
+from tinydb import TinyDB, Query
 from subprocess import run
 from guizero import App, Text, Combo, CheckBox, ButtonGroup, PushButton, info
-
-#def do_booking():
-#    info("Booking", "Thank you for booking with us")
-#    print( film_choice.value )
-#    print( vip_seat.value )
-#    print( row_choice.value )
 
 def print_hello():
     run(["echo", "Hello World"])
@@ -14,17 +9,10 @@ def list_all():
     run(["ls", "-al"], shell=True)
 def exit_program():
     sys.exit()
-  
+
+db = TinyDB('db.json')
+
 app = App(title="LessonBox", width=1024, height=768, layout="grid")
-
-#film_description = Text(app, text="Which film?", grid=[0,0], align="left")
-#film_choice = Combo(app, options=["Star Wars", "Frozen", "Lion King"], grid=[1,0], align="left")
-
-#vip_description = Text(app, text="Seat type", grid=[0,1], align="left")
-#vip_seat = CheckBox(app, text="VIP seat?", grid=[1,1], align="left")
-
-#row_description = Text(app, text="Seat location", grid=[0,2], align="left")
-#row_choice = ButtonGroup(app, options=[ ["Front","F"],["Middle","M"],["Back","B"] ], selected="M", horizontal=True, grid=[1,2], align="left")
 
 btn_print_hello = PushButton(app, command=print_hello, text="Say Something", grid=[0,1], align="left")
 btn_list_dir = PushButton(app, command=list_all, text="List Dir", grid=[1,1], align="left")
