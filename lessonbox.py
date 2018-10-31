682e0bae7d9e8b4613f30cf20723515026555ab3
bsantero <brian.santero@gmail.com>
2018-10-31 12:29:05 -0400

    Added DB and some DB methods, rudimentary labels

--- lessonbox.py
+++ lessonbox.py
@@ -1,21 +1,64 @@
-import sys
+from tkinter import *
 from tinydb import TinyDB, Query
 from subprocess import run
-from guizero import App, Text, Combo, CheckBox, ButtonGroup, PushButton, info
 
-def print_hello():
-    run(["echo", "Hello World"])
-def list_all():
-    run(["ls", "-al"], shell=True)
-def exit_program():
-    sys.exit()
+def printHello():
+  run(["echo", "Hello World"])
+def listAll():
+  run(["ls", "-al"])
+def purgeDB():
+	db.purge()
+def quitApp():
+  root.destroy()
+def allStudents(l):
+	global names
+	n_components = db.all()
+	names = ''
+	for i in n_components:
+		names = names + i['name'] + ', '
+	l.config(text=names)
+	print (names)
 
 db = TinyDB('db.json')
+root = Tk()
 
-app = App(title="LessonBox", width=1024, height=768, layout="grid")
+#FullScreen
+#root.overrideredirect(True)
+#root.overrideredirect(False)
+#root.attributes('-fullscreen',True)
 
-btn_print_hello = PushButton(app, command=print_hello, text="Say Something", grid=[0,1], align="left")
-btn_list_dir = PushButton(app, command=list_all, text="List Dir", grid=[1,1], align="left")
-btn_exit_prgm = PushButton(app, command=exit_program, text="Exit", grid=[3,0], align="right")
-                         
-app.display()
+root.geometry("1024x576")
+
+#Database Testing
+db.purge()
+db.insert({'id':0, 'name':'Jadie Kordan', 'student':True, 'firstDate': '8/29/16', 'school':['Curtis','Juilliard']})
+db.insert({'id':1, 'name':'Oretnas Nairb', 'student':False, 'firstDate': '1/10/15', 'school':'none'})
+db.insert({'id':2, 'name':'Vaughn der Alex', 'student':True, 'firstDate': '8/28/18', 'school':['Oberlin','Curtis','Juilliard']})
+
+numInDb = len(db)
+index = 1
+bool = True
+User = Query()
+Student = db.search(User['id'] == index)
+print(Student[0])
+
+#Gui Elements
+labelStudentName = Label(root, text=Student[0]['name'])
+labelDbNum = Label(root, text=numInDb)
+quit = Button(root, text = 'QUIT', command = quitApp)
+printHello = Button(root, command=printHello, text="Say Something")
+exitPrgm = Button(root, command=quitApp, text="Exit")
+listDir = Button(root, command=listAll, text="List Dir")
+labelDbAll = Label(root, fg="green")
+allStudents(labelDbAll)
+     
+#Gui Elements Properties
+quit.grid(row=0, column=0)
+labelDbNum.grid(row=0, column=1)
+labelStudentName.grid(row=1, column=0, columnspan=2)
+printHello.grid(row=2, column=0)
+listDir.grid(row=2, column=1)
+labelDbAll.grid(row=3,column=1,columnspan=2)
+
+#Run Main Loop
+root.mainloop()
